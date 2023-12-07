efa3b0bd8303707ac98225bba84351ae3fc21e8e
Gabriel Zúñiga <gabrielz6797@gmail.com>
2023-12-06 19:12:41 -0600

    Added firebase libraries

---
+++ Proyecto/space_battles/lib/models/player_data.dart
@@ -0,0 +1,9 @@
+class PlayerData {
+  late String name;
+  late int chosenShip;
+
+  PlayerData() {
+    name = 'hola';
+    chosenShip = 0;
+  }
+}
--- Proyecto/space_battles/lib/screens/game_play.dart
+++ Proyecto/space_battles/lib/screens/game_play.dart
@@ -1,11 +1,19 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:space_battles/game/game.dart';
+import 'package:space_battles/models/player_data.dart';
 import 'package:space_battles/widgets/overlays/pause_button.dart';
 import 'package:space_battles/widgets/overlays/pause_menu.dart';
 
-class GamePlay extends StatelessWidget {
+class GamePlay extends StatefulWidget {
   const GamePlay({super.key});
+
+  @override
+  State<GamePlay> createState() => _GamePlayState();
+}
+
+class _GamePlayState extends State<GamePlay> {
+  PlayerData playerData = PlayerData();
 
   @override
   Widget build(BuildContext context) {
