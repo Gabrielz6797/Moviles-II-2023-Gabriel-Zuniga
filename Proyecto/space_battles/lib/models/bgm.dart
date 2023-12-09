99e3cb1761db1d26e6fa08a4ae50e9f450ad0117
Gabriel Zúñiga <gabrielz6797@gmail.com>
2023-12-09 09:49:15 -0600

    Changed BGM format and added APK

--- Proyecto/space_battles/lib/models/bgm.dart
+++ Proyecto/space_battles/lib/models/bgm.dart
@@ -10,21 +10,21 @@ class BGM {
 
   static final bgms = <BGM>[
     BGM(bgmID: 0, name: 'No BGM'),
-    BGM(bgmID: 1, name: '1. Neon Road Trip.wav'),
-    BGM(bgmID: 2, name: '2. Spaceman.wav'),
-    BGM(bgmID: 3, name: '3. Psychedelia Flashback.wav'),
-    BGM(bgmID: 4, name: '4. No Pressure Trance.wav'),
-    BGM(bgmID: 5, name: '5. Romance on Neon.wav'),
-    BGM(bgmID: 6, name: '6. 80s, Disco, Life.wav'),
-    BGM(bgmID: 7, name: '7. The SynthRave.wav'),
-    BGM(bgmID: 8, name: '8. ZoomBeats.wav'),
-    BGM(bgmID: 9, name: '9. Space Invaders.wav'),
-    BGM(bgmID: 10, name: '10. Laser Party.wav'),
-    BGM(bgmID: 11, name: '11. Flanger Party.wav'),
-    BGM(bgmID: 12, name: '12. SynthBomb.wav'),
-    BGM(bgmID: 13, name: '13. Night Life.wav'),
-    BGM(bgmID: 14, name: '14. Retro Noir.wav'),
-    BGM(bgmID: 15, name: '15. Trickle Bass.wav'),
+    BGM(bgmID: 1, name: '1. Neon Road Trip.ogg'),
+    BGM(bgmID: 2, name: '2. Spaceman.ogg'),
+    BGM(bgmID: 3, name: '3. Psychedelia Flashback.ogg'),
+    BGM(bgmID: 4, name: '4. No Pressure Trance.ogg'),
+    BGM(bgmID: 5, name: '5. Romance on Neon.ogg'),
+    BGM(bgmID: 6, name: '6. 80s, Disco, Life.ogg'),
+    BGM(bgmID: 7, name: '7. The SynthRave.ogg'),
+    BGM(bgmID: 8, name: '8. ZoomBeats.ogg'),
+    BGM(bgmID: 9, name: '9. Space Invaders.ogg'),
+    BGM(bgmID: 10, name: '10. Laser Party.ogg'),
+    BGM(bgmID: 11, name: '11. Flanger Party.ogg'),
+    BGM(bgmID: 12, name: '12. SynthBomb.ogg'),
+    BGM(bgmID: 13, name: '13. Night Life.ogg'),
+    BGM(bgmID: 14, name: '14. Retro Noir.ogg'),
+    BGM(bgmID: 15, name: '15. Trickle Bass.ogg'),
   ];
 
   BGM.fromMap(Map<String, dynamic> map)
@@ -33,7 +33,7 @@ class BGM {
 
   static Map<String, dynamic> defaultData = {
     'bgmID': 1,
-    'name': '1. Neon Road Trip.wav',
+    'name': '1. Neon Road Trip.ogg',
   };
 
   static List<String> getNames(List<BGM> bgms) {
